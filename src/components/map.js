fe9a81f948f607d0abe04f48626e4ecb1debf333
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-02-27 16:19:53 -0600

    Install leaflet

--- src/components/map.js
+++ src/components/map.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Icon } from 'leaflet'
+import markerIcon from '../images/icon-location.svg'
 
-const Map = () => {
+const Map = ({data}) => {
+  const [position, setPosition] = useState([51.505, -0.09])
+
+  const locationIcon = new Icon({
+    iconUrl: markerIcon,
+    iconSize: [30, 40],
+  })
+
+  useEffect(() => {
+    if (data) {
+      setMapPosition(data.lat, data.long)
+    }
+  }, [data])
+
+  const setMapPosition = (lat, long) => {
+    setPosition([lat, long])
+  }
+
   return (
     <div className="map-wrapper">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={position} icon={locationIcon}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
