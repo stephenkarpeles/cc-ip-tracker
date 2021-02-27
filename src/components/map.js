ef1711bd97bd1b5b6864e002e08b297a306b8759
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-02-27 15:34:16 -0600

    Dynamic marker position

--- src/components/map.js
+++ src/components/map.js
@@ -4,12 +4,12 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 const Map = () => {
   return (
     <div className="map-wrapper">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={position}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
