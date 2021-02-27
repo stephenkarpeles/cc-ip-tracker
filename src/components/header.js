9e6de2c5832ad18cd95735927bc4e919b67eb13f
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-02-27 11:52:44 -0600

    Set state on search input

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
+
+  const [searchValue, setSearchValue] = useState('')
+
   return (
     <div className="header">
-      Header
+      <h1>IP Address Tracker</h1>
+
+      <div className='search-box'>
+        <input  
+          value={searchValue}
+          onChange={handleChange}        
+          placeholder='Search for any IP address or domain'
+          aria-labelledby='search'          
+          aria-label='Search for any IP address or domain'
+          type='text'
+          name='search'          
+        />
+
+        <button className='search-box__btn' onClick={handleClick}>
+          <img src={require('../images/icon-arrow.svg')} alt='icon arrow' />
+        </button>
+      </div>
     </div>
   )
 }
